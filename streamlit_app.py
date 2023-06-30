664f6cca25bfe4f3c9cabf2efefb8238919313f2
Kavy-gupta <138204909+Kavy-gupta@users.noreply.github.com>
2023-06-30 14:40:40 +0530

    Update streamlit_app.py
    add breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('my parents new healthy dinner')
-
+print('Breakfast menu')
+print('omega 3 and blueberry oatmeal')
+print('Kale spinach and rocket smoothie')
+print('hard boiled free-ranged egg')
