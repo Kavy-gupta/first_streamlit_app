032d215ccf724091d1a24f1358cff63ab8a15854
Kavy-gupta <138204909+Kavy-gupta@users.noreply.github.com>
2023-06-30 14:46:21 +0530

    Update streamlit_app.py
    added itmes and emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('my parents new healthy dinner')
-streamlit.header('Breakfast menu')
-streamlit.text('omega 3 and blueberry oatmeal')
-streamlit.text('Kale spinach and rocket smoothie')
-streamlit.text('hard boiled free-ranged egg')
+streamlit.header('Breakfast Favorite')
+streamlit.text('🥣 omega 3 and blueberry oatmeal')
+streamlit.text('🥗 Kale spinach and rocket smoothie')
+streamlit.text('🐔 hard boiled free-ranged egg')
+streamlit.text('🥑🍞 avcoda toast')
