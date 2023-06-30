541fe38c99cabb7f2389cf6e86d6110322cf53f9
Kavy-gupta <138204909+Kavy-gupta@users.noreply.github.com>
2023-06-30 14:49:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-streamlit.title('My mom's new healthy dinner')
+streamlit.title('My mom"'"s new healthy dinner')
 streamlit.header('Breakfast Favorite')
 streamlit.text('🥣 omega 3 and blueberry oatmeal')
 streamlit.text('🥗 Kale spinach and rocket smoothie')
 streamlit.text('🐔 hard boiled free-ranged egg')
 streamlit.text('🥑🍞 avcoda toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
