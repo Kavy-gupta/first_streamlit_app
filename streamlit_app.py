7d173a73685f31b7942ebe3209c248ed68852a0c
Kavy-gupta <138204909+Kavy-gupta@users.noreply.github.com>
2023-07-24 12:04:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,10 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityive api response
 streamlit.header('Fruityvice Fruit Advicde!')
-
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 streamlit.text(fruityvice_response)
 
 # write your own comment -what does the next line do? 
