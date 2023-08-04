60cc41022f992eb9d15cc21086ffb3e918f8de43
Kavy-gupta <138204909+Kavy-gupta@users.noreply.github.com>
2023-08-04 16:09:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,7 +85,7 @@ import streamlit as st
 
 df = pd.read_csv('https://raw.githubusercontent.com/Kavy-gupta/first_streamlit_app/main/veg_plant_height.csv')
 #st.dataframe(df)
-fig=plt.bar(df,x=plant_name,y=Low_End_of_Range,orientation="h",)
-st.bar_chart(fig)
+#fig=plt.bar(df,x=plant_name,y=Low_End_of_Range,orientation="h",)
+st.bar_chart(df['Low_End_of_Range'])
 
 
